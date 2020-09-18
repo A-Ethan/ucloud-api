007b5545a9af2ed4f8af9a89d405cc6fb280d3eb
A-Ethan <ethan.shen@ucloud.cn>
2020-09-18 16:06:11 +0800

    add more

--- get_cube_extend_info.go
+++ get_cube_extend_info.go
@@ -6,7 +6,6 @@ Examples: https://github.com/ucloud/ucloud-sdk-go/tree/master/examples
 package main
 
 import (
-	"encoding/base64"
 	"fmt"
 	"os"
 
@@ -23,39 +22,16 @@ func main() {
 	cred.PublicKey = os.Getenv("UCLOUD_API_PUBLICKEY")
 	cred.PrivateKey = os.Getenv("UCLOUD_API_PRIVATEKEY")
 
-	pod := `apiVersion: v1beta1
-kind: Pod
-spec:
-  containers:
-  - name: cube01
-    image: 'uhub.service.ucloud.cn/hello321/python:3.7.7-alpine'
-    resources:
-      limits:
-        memory: 1024Mi
-        cpu: 1000m
-    volumeMounts: []
-  volumes: []
-  imagePullSecrets:
-  - password: xxxx
-    registryserver: uhub.service.ucloud.cn
-    username: ethan.shen@ucloud.cn
-  restartPolicy: Always`
-	podBase := base64.StdEncoding.EncodeToString([]byte(pod))
-
-	// fmt.Println("[Pod]", podBase)
-
 	// New Client
 	Client := ucloud.NewClient(&cfg, &cred)
 
 	req := Client.NewGenericRequest()
 	err := req.SetPayload(map[string]interface{}{
-		"Action":    "CreateCubePod",
+		"Action":    "GetCubeExtendInfo",
 		"Region":    "cn-bj2",
 		"Zone":      "cn-bj2-02",
 		"ProjectId": "org-znwjjr",
-		"VPCId":     "uvnet-q1anvtk2",
-		"SubnetId":  "subnet-r0oxidkt",
-		"Pod":       podBase,
+		"CubeIds":   "cube-cdrb4hfe,cube-4mqltheu",
 	})
 
 	if err != nil {
@@ -72,3 +48,5 @@ spec:
 
 	fmt.Println("[RESPONSE]", resp)
 }
+
+
