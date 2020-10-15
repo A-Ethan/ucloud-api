b956de5bf4df68f47178cccc2daf4c77a938f67d
A-Ethan <ethan.shen@ucloud.cn>
2020-10-15 13:52:04 +0800

    add DeleteCubePod

---
+++ delete_cube_pod.go
@@ -0,0 +1,51 @@
+/**
+Homepage: https://github.com/ucloud/ucloud-sdk-go
+Examples: https://github.com/ucloud/ucloud-sdk-go/tree/master/examples
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ucloud/ucloud-sdk-go/ucloud"
+	"github.com/ucloud/ucloud-sdk-go/ucloud/auth"
+)
+
+func main() {
+	cfg := ucloud.NewConfig()
+	cfg.Region = "cn-bj2"
+	cfg.BaseUrl = "https://api.ucloud.cn"
+
+	cred := auth.NewCredential()
+	cred.PublicKey = os.Getenv("UCLOUD_API_PUBLICKEY")
+	cred.PrivateKey = os.Getenv("UCLOUD_API_PRIVATEKEY")
+
+	// New Client
+	Client := ucloud.NewClient(&cfg, &cred)
+
+	req := Client.NewGenericRequest()
+	err := req.SetPayload(map[string]interface{}{
+		"Action":     "DeleteCubePod",
+		"Region":     "cn-bj2",
+		"Zone":       "cn-bj2-02",
+		"ProjectId":  "org-znwjjr",
+		"CubeId":     "cube-mzoz4g4q",
+		"ReleaseEIP": "True",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	// send request
+	resp, err := Client.GenericInvoke(req)
+
+	if err != nil {
+		fmt.Println("[ERROR]", err)
+		return
+	}
+
+	fmt.Println("[RESPONSE]", resp)
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/A-Ethan/ucloud-api
 
 go 1.13
 
-require github.com/ucloud/ucloud-sdk-go v0.17.1
+require (
+	github.com/ucloud/ucloud-sdk-go v0.17.1
+	golang.org/x/tools/gopls v0.5.0 // indirect
+)
